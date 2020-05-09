60a63ab9bd2ba13fc967ad14d4555c6df18ff69a
KadyrovAmir <amir.kadyrov.97@mail.ru>
2020-05-09 21:11:25 +0300

    added graph displaying

--- app.py
+++ app.py
@@ -14,5 +14,15 @@ def main_app():
     return render_template('main.html', list=output_list)
 
 
+@app.route('/image')
+def get_image():
+    rad_list = wall_radiation(10, 10, 13, 1, 5, 6, 1, 1)
+    x, z, radiation = [], [], []
+    for rad in rad_list:
+        x.append(rad.x)
+        z.append(rad.z)
+        radiation.append(rad.rad)
+    return render_template('image.html', x=x, z=z, rad=radiation)
+
 if __name__ == '__main__':
     app.run(debug=True)
